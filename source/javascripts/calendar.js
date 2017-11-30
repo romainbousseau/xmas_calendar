5d9b0a807fc68f0a1c711f81ee4795cd6bda456b
Romain Bousseau <romainbousseau1@gmail.com>
2017-11-30 12:57:16 +0100

    play audio only on 24th

--- source/javascripts/calendar.js
+++ source/javascripts/calendar.js
@@ -3,11 +3,15 @@ $(document).ready(function(){
     // Change '<' in '>' before lauch ;)
     if($('.calendar-container').data('day') < moment().format()){
       $($(this).data("target")).addClass("active");
+      if($(this).data('date') == "2017-12-24") {
+        $('#sinatra')[0].play();
+      }
     } else {
       $('#stalker').addClass('active');
     }
   })
   $(".modal-background").click(function() {
+    $('#sinatra')[0].pause();
     $('.modal').removeClass('active');
     $.ajax({
       url: "http://thecatapi.com/api/images/get?format=src&type=gif"
