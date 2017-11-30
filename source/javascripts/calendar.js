c2584d59a1830879142d2f0b79ddcf71afd4c04b
Romain Bousseau <romainbousseau1@gmail.com>
2017-11-30 11:33:44 +0100

    stalkers modal with random-cats

--- source/javascripts/calendar.js
+++ source/javascripts/calendar.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
   $('.day-container').click(function() {
     // Change '<' in '>' before lauch ;)
-    if($('.calendar-container').data('day') < moment().format()){
+    if($('.calendar-container').data('day') > moment().format()){
       $($(this).data("target")).addClass("active");
     } else {
-      alert("La curiosité est un vilain défaut !")
+      $('#stalker').addClass('active');
     }
   })
   $(".modal-background").click(function() {
     $('.modal').removeClass('active');
+    $.ajax({
+      url: "http://thecatapi.com/api/images/get?format=src&type=gif"
+    })
+    $("#random-cat").attr("src","http://thecatapi.com/api/images/get?format=src&type=gif");
   })
 });
